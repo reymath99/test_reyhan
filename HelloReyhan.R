63309d713b625b204934d91354c5ba5f8557d573
reymath99 <aansum99@gmail.com>
2022-02-07 10:03:18 +0700

    ada tambahan say hbd

--- HelloReyhan.R
+++ HelloReyhan.R
@@ -1,4 +1,7 @@
-hellox<-function(x){
+hellox<-function(x,tahun_lahir){
   paste0("Hello"," ",x)
+  # tambah ucapan say hbd
+  paste0("Selamat Ulang Tahun ke"," ",(2021-tahun_lahir))
+  
 }
-hellox("Reyhan")
+hellox("Reyhan",1999)
